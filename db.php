c1746feb7fda5d3bc41d02bccc308a409aa23558
Flavius-George <151860751+Flavius-George@users.noreply.github.com>
2025-10-16 19:50:43 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
-$servername = "db";
-$username = "user";
-$password = "pass";
+$servername = "mysql"; // sau "localhost" dacă e același container
+$username = "root";
+$password = "root_password"; // parola ta
 $dbname = "fs177";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
-    die("Conexiune esuata: " . $conn->connect_error);
+    die("Conexiunea a eșuat: " . $conn->connect_error);
 }
 ?>
-
-
